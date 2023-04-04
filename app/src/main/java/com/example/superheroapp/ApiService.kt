f6e63eb88a87ca6fbc4981cc5476e1a4c295e751
joaquinxtx <thejotex1998@gmail.com>
2023-04-03 22:58:09 -0300

    [second commit] implement Retrofit

---
+++ app/src/main/java/com/example/superheroapp/ApiService.kt
@@ -0,0 +1,12 @@
+package com.example.superheroapp
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET("905507053900621/search/{name}")
+    suspend fun getSuperHeroes(@Path("name") superHeroName:String):Response<SuperHeroDataResponse>
+
+}
--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.SearchView
+import com.example.superheroapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var retrofit: Retrofit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        retrofit=getRetrofit()
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+        })
+    }
+
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val myResponse=retrofit.create(ApiService::class.java).getSuperHeroes(query)
+            if (myResponse.isSuccessful){
+                val response = myResponse.body()
+                Log.i("jota",response.toString())
+            }else
+                Log.i("jota","no funca")
+        }
+    }
+    private fun getRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://superheroapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
